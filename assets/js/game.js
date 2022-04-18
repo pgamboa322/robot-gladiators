7e688dae94325b7a2fffc82fa4a662e467128f2f
pgamboa322 <pgamboa322@gmail.com>
2022-04-18 12:02:56 -0500

    enemy rounds added

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,9 +14,12 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    //repeat and execute as long as the enemy robot is alive
+    while(enemyHealth > 0) {
     
+    // Alert players that they are starting the round
+    //window.alert("Welcome to Robot Gladiators!");
+
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   
     // if player choses to fight, then fight
@@ -65,6 +68,9 @@ var fight = function(enemyName) {
     window.alert("You need to choose a valid option. Try again!");
   }
 }
+}
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
